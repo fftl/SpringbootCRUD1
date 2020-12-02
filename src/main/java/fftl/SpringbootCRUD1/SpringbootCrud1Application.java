ac1fc4a296fdf580bb82b4634a398f62e0f1745b
fftl <fftl.0710@gmail.com>
2020-12-02 12:24:03 +0900

    201202, 12:23, SpringbootCRUD1 프로젝트 생성

---
+++ src/main/java/fftl/SpringbootCRUD1/SpringbootCrud1Application.java
@@ -0,0 +1,13 @@
+package fftl.SpringbootCRUD1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrud1Application.class, args);
+	}
+
+}
---
+++ src/test/java/fftl/SpringbootCRUD1/SpringbootCrud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fftl.SpringbootCRUD1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrud1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
